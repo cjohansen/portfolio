ed58fccac6f0c2296a9e25c1befd835dc3d6b271
Christian Johansen <christian@cjohansen.no>
2022-10-11 22:54:15 +0200

    Add rudimentary zoom tools

--- src/portfolio/kitchen_sink.cljs
+++ src/portfolio/kitchen_sink.cljs
@@ -3,10 +3,11 @@
   (:require [portfolio.client :as client]
             [portfolio.core :as portfolio]
             [portfolio.views.canvas :as canvas]
+            [portfolio.views.canvas.args :as canvas-args]
             [portfolio.views.canvas.background :as canvas-bg]
             [portfolio.views.canvas.grid :as canvas-grid]
             [portfolio.views.canvas.viewport :as canvas-vp]
-            [portfolio.views.canvas.args :as canvas-args]))
+            [portfolio.views.canvas.zoom :as canvas-zoom]))
 
 (defn create-app [config]
   (-> (portfolio/init-state config)
@@ -14,7 +15,10 @@
                       {:canvas/layout (:canvas/layout config)
                        :tools [(canvas-bg/create-background-tool config)
                                (canvas-vp/create-viewport-tool config)
-                               (canvas-grid/create-grid-tool config)]
+                               (canvas-grid/create-grid-tool config)
+                               (canvas-zoom/create-zoom-in-tool config)
+                               (canvas-zoom/create-zoom-out-tool config)
+                               (canvas-zoom/create-reset-zoom-tool config)]
                        :addons [(canvas-args/create-args-panel config)]})])
       atom))
 
---
+++ src/portfolio/views/canvas/zoom.cljs
@@ -0,0 +1,64 @@
+(ns portfolio.views.canvas.zoom
+  (:require [portfolio.components.canvas :as canvas]
+            [portfolio.components.canvas-toolbar-buttons :refer [Button]]
+            [portfolio.protocols :as portfolio]
+            [portfolio.views.canvas.addons :as addons]
+            [portfolio.views.canvas.protocols :as protocols]))
+
+(defn zoom [el opt]
+  (when-let [lvl (:zoom/level opt)]
+    (let [body (canvas/get-iframe-body el)
+          size (str (/ 100 lvl) "%")]
+      (set! (.. body -style -transform) (str "scale(" lvl ")"))
+      (set! (.. body -style -transformOrigin) "left top")
+      (set! (.. body -style -width) size)
+      (set! (.. body -style -height) size))))
+
+(defn prepare-zoom-button [tool state {:keys [pane-id pane-options]}]
+  (let [level (or (:zoom/level pane-options) 1)
+        increment (or (:zoom-increment tool) 0.25)]
+    (with-meta
+      {:text (:title tool)
+       :active? (if (< 0 increment)
+                  (< 1 level)
+                  (< level 1))
+       :actions [[:assoc-in
+                  [(:id tool) pane-id :value :zoom/level]
+                  (+ increment level)]]}
+      {`protocols/render-toolbar-button #'Button})))
+
+(def impl
+  {`portfolio/prepare-layer (fn [_ el opt] (zoom el opt))
+   `protocols/prepare-toolbar-button #'prepare-zoom-button})
+
+(defn create-zoom-in-tool [config]
+  (with-meta
+    {:id :canvas/zoom
+     :title "Zoom in"
+     :zoom-increment (or (:zoom-increment config) 0.25)}
+    impl))
+
+(defn create-zoom-out-tool [config]
+  (with-meta
+    {:id :canvas/zoom
+     :title "Zoom out"
+     :zoom-increment (or (:zoom-increment config) -0.25)}
+    impl))
+
+(defn reset-canvas-zoom [data el opt]
+  (when-not (contains? opt :zoom/level)
+    (let [body (canvas/get-iframe-body el)]
+      (set! (.. body -style -transform) "")
+      (set! (.. body -style -width) "100%")
+      (set! (.. body -style -height) "100%"))))
+
+(defn create-reset-zoom-tool [config]
+  (addons/create-action-button
+   {:id :canvas/zoom-reset
+    :title "Reset zoom"
+    :prepare-layer #'reset-canvas-zoom
+    :get-actions (fn [_ _ {:keys [pane-id]}]
+                   [[:dissoc-in [:canvas/zoom pane-id :value :zoom/level]]])
+    :show? (fn [_ _ {:keys [pane-options]}]
+             (and (:zoom/level pane-options)
+                  (not= 1 (:zoom/level pane-options))))}))
