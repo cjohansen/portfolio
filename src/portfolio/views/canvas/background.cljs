6c4de063b7b85d6285a9e2e0cdce9c0fc7c7786f
Christian Johansen <christian@cjohansen.no>
2022-11-16 08:37:47 +0100

    Default light mode to _almost_ white

--- src/portfolio/views/canvas/background.cljs
+++ src/portfolio/views/canvas/background.cljs
@@ -5,7 +5,7 @@
 (def default-options
   [{:id :light-mode
     :title "Light (.light-mode)"
-    :value {:background/background-color "#fff"
+    :value {:background/background-color "#f8f8f8"
             :background/body-class "light-mode"}}
    {:id :dark-mode
     :title "Dark (.dark-mode)"
