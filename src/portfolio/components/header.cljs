6923c510645c1c843a1763b4c78d18667212a6cd
Christian Johansen <christian@cjohansen.no>
2022-11-25 07:18:34 +0100

    Add missing namespace

---
+++ src/portfolio/components/header.cljs
@@ -0,0 +1,22 @@
+(ns portfolio.components.header
+  (:require [dumdom.core :as d]
+            [portfolio.icons.hamburger :as hamburger]))
+
+(d/defcomponent Header [{:keys [title actions]}]
+  [:div {:style {:display "flex"
+                 :padding "0 20px"
+                 :transition "height 0.25s ease"
+                 :height 0
+                 :overflow "hidden"
+                 :align-items "center"}
+         :mounted-style {:height 35}
+         :leaving-style {:height 0}}
+   [:div {:style {:margin-right 10
+                  :margin-top 10
+                  :width 24
+                  :height 24
+                  :cursor "pointer"}
+          :on-click actions}
+    hamburger/icon]
+   [:h1.h2 {:style {:margin-top 10}}
+    title]])
