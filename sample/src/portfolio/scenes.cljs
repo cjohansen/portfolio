08452c7648e95cc1102ae62f694b79ad75295c2a
Christian Johansen <christian@cjohansen.no>
2023-02-24 15:02:08 +0100

    Update sample namespace

--- sample/src/portfolio/scenes.cljs
+++ sample/src/portfolio/scenes.cljs
@@ -12,19 +12,25 @@
             [portfolio.ui :as ui]))
 
 (ui/start!
- {:config {:css-paths ["/portfolio/demo.css"]
-           ;;:canvas/layout [[{:background/background-color "#f0f0f9"}]]
+ {:config
+  {:css-paths ["/portfolio/demo.css"]
+   :canvas/multi-scene-defaults {:viewport/width 390
+                                 :viewport/height 400}
 
-           :background/options
-           [{:id :light-mode
-             :title "Bright mode (.bright-mode)"
-             :value {:background/background-color "#f8f8f8"
-                     :background/body-class "light-mode"}}
-            {:id :dark-mode
-             :title "Bleak mode (.bleak-mode)"
-             :value {:background/background-color "#000000"
-                     :background/body-class "dark-mode"}}]
+   :viewport/defaults {:viewport/padding [0 0 0 0]}
+   :canvas/layout [[{:viewport/width 390
+                     :viewport/height 400}]]
+   ;;:canvas/layout [[{:background/background-color "#ff3300 "}]]
 
-           :background/default-option-id :dark-mode
+   ;; :background/options
+   ;; [{:id :light-mode
+   ;;   :title "Bright mode (.bright-mode)"
+   ;;   :value {:background/background-color "#f8f8f8"
+   ;;           :background/body-class "light-mode"}}
+   ;;  {:id :dark-mode
+   ;;   :title "Bleak mode (.bleak-mode)"
+   ;;   :value {:background/background-color "#000000"
+   ;;           :background/body-class "dark-mode"}}]
 
-           }})
+   ;; :background/default-option-id :dark-mode
+   }})
