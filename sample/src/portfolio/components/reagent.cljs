5a2f466645119229a61e508eca75e476bb63d30a
Brandon Stubbs <brandon@bos-solutions.co.uk>
2023-03-29 16:42:07 -0400

    link metosins article on the new api

--- sample/src/portfolio/components/reagent.cljs
+++ sample/src/portfolio/components/reagent.cljs
@@ -3,6 +3,7 @@
    ;; If you are using an older version of react use the following:
    #_[portfolio.reagent :refer-macros [defscene]]
    ;; For react versions 18+ use the following:
+   ;; This is due to the new API https://www.metosin.fi/blog/reagent-towards-react-18/
    [portfolio.reagent-18 :refer-macros [defscene]]))
 
 (defn button [text]
