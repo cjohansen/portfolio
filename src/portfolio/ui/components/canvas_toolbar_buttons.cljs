a28a674f5336c49379414f886558f8413c6a7335
Christian Johansen <christian@cjohansen.no>
2023-04-07 22:06:31 +0200

    Minor visual tweaks

--- src/portfolio/ui/components/canvas_toolbar_buttons.cljs
+++ src/portfolio/ui/components/canvas_toolbar_buttons.cljs
@@ -4,28 +4,29 @@
             [portfolio.ui.canvas.protocols :as protocols]
             [portfolio.ui.components.popup-menu :refer [PopupMenu]]))
 
-(d/defcomponent MenuButton [{:keys [text icon title menu active? actions]}]
-  [:span.canvas-menu-button
-   {:style {:display "flex"
-            :position "relative"}}
-   [:button.button.boldable
-    {:title (or title text)
-     :style {:color (if menu "#1ea7fd" "var(--fg)")
-             :display "block"
-             :font-weight "bold"
-             :font-size 14
-             :padding (if icon 6 "6px 12px")
-             :width (when icon 32)
-             :height 32
-             :background (if menu
-                           "var(--toolbar-button-active)"
-                           "var(--toolbar-button)")
-             }
-     :on-click actions}
-    (when icon
-      (icons/render-icon icon {:size 20}))
-    text]
-   (some-> menu PopupMenu)])
+(d/defcomponent MenuButton [{:keys [text icon title menu active? actions selected?]}]
+  (let [selected? (or menu selected?)]
+    [:span.canvas-menu-button
+     {:style {:display "flex"
+              :position "relative"}}
+     [:button.button.boldable
+      {:title (or title text)
+       :style {:color (if selected? "var(--highlight-color)" "var(--fg)")
+               :display "block"
+               :font-weight "bold"
+               :font-size 14
+               :padding (if icon 6 "6px 12px")
+               :width (when icon 32)
+               :height 32
+               :background (if selected?
+                             "var(--toolbar-button-active)"
+                             "var(--toolbar-button)")
+               }
+       :on-click actions}
+      (when icon
+        (icons/render-icon icon {:size 20}))
+      text]
+     (some-> menu PopupMenu)]))
 
 (def Button MenuButton)
 
