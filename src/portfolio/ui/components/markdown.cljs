45fc9ee3835a79aeb09a783b8f9b28a7db146d06
Christian Johansen <christian@cjohansen.no>
2023-04-17 16:52:33 +0200

    Support clj and cljs as aliases for Clojure code in MD blocks

--- src/portfolio/ui/components/markdown.cljs
+++ src/portfolio/ui/components/markdown.cljs
@@ -1,12 +1,16 @@
 (ns portfolio.ui.components.markdown
   (:require [dumdom.core :as d]))
 
+(def langs
+  {"clj" "clojure"
+   "cljs" "clojure"})
+
 (d/defcomponent Markdown
   :on-render (fn [el props]
                (doseq [pre (.querySelectorAll el "pre")]
                  (let [code (.-firstElementChild pre)]
                    (when (= "CODE" (.-tagName code))
-                     (set! (.-className pre) (str "language-" (.-className code)))
+                     (set! (.-className pre) (str "language-" (or (langs (.-className code)) (.-className code))))
                      (js/Prism.highlightElement pre)))))
   [{:keys [markdown]}]
   [:div.md {:dangerouslySetInnerHTML {:__html markdown}}])
