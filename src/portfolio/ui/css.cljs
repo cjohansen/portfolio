f363b0a80c0ca1ea1ab1fffba3c035fc911ec7ba
Christian Johansen <christian@cjohansen.no>
2023-04-20 11:10:18 +0200

    Don't inadvertantly rely on very new cljs

--- src/portfolio/ui/css.cljs
+++ src/portfolio/ui/css.cljs
@@ -3,6 +3,7 @@
 
 (defn find-link-by-href [el path]
   (->> (.querySelectorAll el "link")
+       array-seq
        (filter #(str/includes? (.-href %) path))
        first))
 
@@ -17,7 +18,7 @@
     link))
 
 (defn reload-css-file [file]
-  (doseq [iframe (.querySelectorAll js/document.body "iframe")]
+  (doseq [iframe (array-seq (.querySelectorAll js/document.body "iframe"))]
     (let [iframe-head (some-> iframe .-contentWindow .-document .-head)
           original (find-link-by-href iframe-head file)
           reloaded (create-css-link (str file "?" (.getTime (js/Date.))))]
@@ -38,9 +39,10 @@
       (.appendChild js/document.head (create-css-link path {:media "portfolio"})))))
 
 (defn replace-loaded-css-files [paths]
-  (doseq [iframe (.querySelectorAll js/document.body "iframe")]
+  (doseq [iframe (array-seq (.querySelectorAll js/document.body "iframe"))]
     (let [head (some-> iframe .-contentWindow .-document .-head)]
       (doseq [link (->> (.querySelectorAll head "link")
+                        array-seq
                         (filter #(.-portfolio %)))]
         (-> link .-parentNode (.removeChild link)))
       (doseq [path paths]
